7f3269ab9c4fe12a5faa5d52ac2f5eace830639b
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-03-27 17:22:24 +0300

    update: get and post methods created

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@
 const express = require("express");
 const app = express()
 
+app.use(express.json())
+
 const db = require('./models')
+
+//Routers
+const postsRouter = require('./routes/Posts')
+app.use('/posts', postsRouter)
 
 db.sequelize.sync().then(()=>{
     app.listen(3001,()=>{
---
+++ server/routes/Posts.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { Posts } = require('../models')
+
+router.get("/", async ( req, res )=>{
+    try{
+        const listPosts = await Posts.findAll()
+        res.json(listPosts)
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+
+router.post("/", async ( req, res )=>{
+    const post = req.body
+    try{
+        await Posts.create(post)
+        res.json(post)
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+
+module.exports = router;

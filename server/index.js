f3d5bccca0d61d6000ce0c43d9a07198c8e66e91
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-03-28 22:59:35 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-
+require('dotenv').config();
 //use
 app.use(express.json());
 app.use(cors());
@@ -22,7 +22,7 @@ app.use('/userAuth', usersRouter);
 
 // code to sql to db
 db.sequelize.sync().then(()=>{
-    app.listen(3001,()=>{
+    app.listen(process.env.PORT || 3001,()=>{
         console.log('server is running on 3001 port...');
     })
 })
